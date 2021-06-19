78b8d497952b63a0c577e4cb43be502662715aa0
Andrew Prokhorenkov <aprokh@uchicago.edu>
2021-06-18 22:58:46 -0500

    feat: rewrite config-local.js

--- docker/config-local.js
+++ docker/config-local.js
@@ -1,15 +1,18 @@
 define([], function () {
 	var configLocal = {};
-
 	// WebAPI
 	configLocal.api = {
-		name: 'OHDSI',
-		url: '${WEBAPI_URL}'
+		name: 'fence',
+		url: 'https://atlas-qa-mickey.planx-pla.net/WebAPI/'
 	};
-
+	configLocal.authProviders = [{
+		"name": "OpenID",
+		"url": "user/login/openid",
+		"ajax": false,
+		"icon": "fa fa-openid"
+	}];
 	configLocal.cohortComparisonResultsEnabled = false;
-	configLocal.userAuthenticationEnabled = false;
+	configLocal.userAuthenticationEnabled = true;
 	configLocal.plpResultsEnabled = false;
-
 	return configLocal;
 });
