3ca1eb722ca02f9f7ecde808b7b6720bd5074a21
tianj7 <jtian1@uchicago.edu>
2023-12-12 15:29:42 -0600

    hide features change

--- docker/config-local.js
+++ docker/config-local.js
@@ -10,6 +10,17 @@ define([], function () {
 	configLocal.cohortComparisonResultsEnabled = false;
 	configLocal.userAuthenticationEnabled = false;
 	configLocal.plpResultsEnabled = false;
+	
+	configLocal.appDataSources = true; 
+	configLocal.appSearch = true;     
+	configLocal.appCohortDefinitions = true;  
+	configLocal.appCharacterizations = true;  
+	configLocal.appCohortPathways = false;    
+	configLocal.appIncidenceRates = false;    
+	configLocal.appProfiles = false;  
+	configLocal.appEstimation = false; 
+	configLocal.appPrediction = false; 
+	configLocal.appReusables = false; 
 
 	return configLocal;
 });
