dc9e2b5fd17f571b6c22e2433bb2e989a342970f
pieterlukasse <pieterlukasse@gmail.com>
2024-05-10 20:30:26 +0200

    feat: introduce a new permission instead of relying on a role for Global sharing
    ...and introduce some other fixes:
    - fix: correct styling for sharing buttons on access modal ui
    - fix: cleanup/remove unnecessary parts
    - fix: adjust permission name to "artifact:global:share:put" to better reflect its real purpose
    - fix: better name for isPermittedGlobalShareArtifact (previous name isPermittedGlobalShareCohort did not reflect the
      fact that it is about all kind of artifacts)
    - fix: move isOwner() check and make sure it is only applied when !config.limitedPermissionManagement
    - fix: move isOwner() check for conceptset-manager

--- js/pages/concept-sets/conceptset-manager.js
+++ js/pages/concept-sets/conceptset-manager.js
@@ -8,8 +8,7 @@ define([
 	'./const',
 	'const',
 	'components/conceptset/utils',
-        'services/Vocabulary',
-    	'services/ShareRoleCheck',
+	'services/Vocabulary',
 	'services/Permission',
 	'services/Tags',
 	'components/security/access/const',
@@ -58,8 +57,7 @@ define([
 	constants,
 	globalConstants,
 	utils,
-        vocabularyAPI,
-        shareRoleCheck,
+	vocabularyAPI,
 	GlobalPermissionService,
 	TagsService,
 	{ entityType },
@@ -187,24 +185,25 @@ define([
 				return this.currentConceptSet() && this.currentConceptSet().id > 0;
 			});
 
-		        this.enablePermissionManagement = ko.observable(false);
- 		        this.enablePermissionManagement(config.enablePermissionManagement);
+			GlobalPermissionService.decorateComponent(this, {
+				entityTypeGetter: () => entityType.CONCEPT_SET,
+				entityIdGetter: () => this.currentConceptSet() && this.currentConceptSet().id,
+				createdByUsernameGetter: () => this.currentConceptSet() && this.currentConceptSet().createdBy
+					&& this.currentConceptSet().createdBy.login
+			});
 
-		        this.userCanShare = ko.observable(false);
-		        if (config.permissionManagementRoleId === "") {
-			   this.userCanShare(true);
-		        } else {
-			   shareRoleCheck.checkIfRoleCanShare(authApi.subject(), config.permissionManagementRoleId)
-				.then(res=>{
-				    this.userCanShare(res);
-				})
-				.catch(error => {
-				    console.error(error);
-				    alert(ko.i18n('conceptSets.conceptSetManager.shareRoleCheck', 'Error when determining if user can share concept sets')());
-				});
-			}		        
+			this.enablePermissionManagement = ko.observable(config.enablePermissionManagement);
+			if (config.enablePermissionManagement) {
+				this.userCanShare = ko.observable(
+					(config.limitedPermissionManagement &&
+					 authApi.isPermittedGlobalShareArtifact()) ||
+					(!config.limitedPermissionManagement &&
+					 this.isOwner())
+				  );
+			} else {
+				this.userCanShare = ko.observable(false);
+			}
 
-		    
 			this.isSaving = ko.observable(false);
 			this.isDeleting = ko.observable(false);
 			this.isOptimizing = ko.observable(false);
@@ -384,13 +383,6 @@ define([
 
 			this.activeUtility = ko.observable("");
 			this.newConceptSetIdForCopyAnnotations = ko.observable(0);
-
-			GlobalPermissionService.decorateComponent(this, {
-				entityTypeGetter: () => entityType.CONCEPT_SET,
-				entityIdGetter: () => this.currentConceptSet() && this.currentConceptSet().id,
-				createdByUsernameGetter: () => this.currentConceptSet() && this.currentConceptSet().createdBy
-					&& this.currentConceptSet().createdBy.login
-			});
 
 			this.tags = ko.observableArray(this.currentConceptSet() && this.currentConceptSet().tags);
 			TagsService.decorateComponent(this, {
