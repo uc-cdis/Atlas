b8e7a8746174c4fddb439aeb4eac45cfdb55425e
pieterlukasse <pieterlukasse@gmail.com>
2025-03-24 20:36:41 +0100

    fix: update user interaction listener to only refresh token if close to expiring

--- js/Application.js
+++ js/Application.js
@@ -99,6 +99,10 @@ define(
 				this.isLoggedIn = ko.computed(() => authApi.isAuthenticated());
 			}
 
+			timeToExpire() {
+				return authApi.tokenExpirationDate() - new Date();
+			}
+
 			/**
 			 * Performs initial setup
 			 * @returns Promise
@@ -145,10 +149,11 @@ define(
 						window.addEventListener(eventType, (event) => {
 							userInteractionCount++;
 							if (userInteractionCount % 30 == 0) {
-								console.log(">>> Refreshing user token....");
+								console.log(">>> Checking user token....");
 								userInteractionCount = 0;
-								// Refresh the Atlas token:
-								if (authApi.isAuthenticated()) {
+								// Refresh the Atlas token if it is close to expiring:
+								if (authApi.isAuthenticated() && this.timeToExpire() < config.refreshTokenThreshold) {
+									console.log(">>> Token close to expiring. Refreshing user token....");
 									authApi.refreshToken();
 								}
 							}
